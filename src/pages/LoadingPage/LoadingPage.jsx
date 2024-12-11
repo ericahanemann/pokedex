b7618a611958328f2ed0aa80c63c1ca0d4febb47
Érica <ericavhanemann@gmail.com>
2024-12-11 11:11:59 -0300

    feat: add loading screen

--- src/pages/LoadingPage/LoadingPage.jsx
+++ src/pages/LoadingPage/LoadingPage.jsx
@@ -1,3 +1,12 @@
+import PokeballIcon from "../../components/PokeballIcon/PokeballIcon";
+import styles from "./LoadingPage.module.css";
+
 export default function LoadingPage() {
-  return <div>Loading...</div>;
+  return (
+    <div className={styles.loadingPageContainer}>
+      <div className={styles.iconContainer}>
+        <PokeballIcon />
+      </div>
+    </div>
+  );
 }
